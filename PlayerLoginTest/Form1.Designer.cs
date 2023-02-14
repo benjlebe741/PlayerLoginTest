6bf8c9d9d918aee19b569d53751424a40cf7f235
benjlebe741 <benjlebe741@ed.amdsb.ca>
2023-02-14 18:21:26 -0500

    Add project files.

---
+++ PlayerLoginTest/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PlayerLoginTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.buttonTimer = new System.Windows.Forms.Timer(this.components);
+            this.difficultyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.Location = new System.Drawing.Point(44, 155);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(197, 121);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "E A S Y";
+            this.easyButton.UseVisualStyleBackColor = true;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            this.easyButton.MouseEnter += new System.EventHandler(this.easyButton_MouseEnter);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.Location = new System.Drawing.Point(280, 155);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(224, 121);
+            this.mediumButton.TabIndex = 1;
+            this.mediumButton.Text = "M E D I U M";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButton_Click);
+            this.mediumButton.MouseHover += new System.EventHandler(this.mediumButton_MouseHover);
+            // 
+            // hardButton
+            // 
+            this.hardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.Location = new System.Drawing.Point(544, 155);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(197, 121);
+            this.hardButton.TabIndex = 2;
+            this.hardButton.Text = "H A R D";
+            this.hardButton.UseVisualStyleBackColor = true;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            this.hardButton.MouseHover += new System.EventHandler(this.hardButton_MouseHover);
+            // 
+            // usernameInput
+            // 
+            this.usernameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.usernameInput.Location = new System.Drawing.Point(294, 83);
+            this.usernameInput.Name = "usernameInput";
+            this.usernameInput.Size = new System.Drawing.Size(197, 23);
+            this.usernameInput.TabIndex = 3;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.usernameLabel.Location = new System.Drawing.Point(307, 36);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(173, 30);
+            this.usernameLabel.TabIndex = 4;
+            this.usernameLabel.Text = "-USERNAME-";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputLabel.Location = new System.Drawing.Point(12, 375);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(776, 66);
+            this.outputLabel.TabIndex = 5;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(280, 298);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(221, 74);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "BEGIN CONQUEST";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // buttonTimer
+            // 
+            this.buttonTimer.Enabled = true;
+            this.buttonTimer.Interval = 500;
+            this.buttonTimer.Tick += new System.EventHandler(this.buttonTimer_Tick);
+            // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.AutoSize = true;
+            this.difficultyLabel.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.difficultyLabel.Location = new System.Drawing.Point(290, 122);
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(207, 20);
+            this.difficultyLabel.TabIndex = 7;
+            this.difficultyLabel.Text = "-Choose your Path-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.difficultyLabel);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.usernameInput);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Timer buttonTimer;
+        private System.Windows.Forms.Label difficultyLabel;
+    }
+}
+
---
+++ PlayerLoginTest/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PlayerLoginTest
+{
+    public partial class Form1 : Form
+    {
+        string chosenDifficulty = "Not chosen";
+        string username;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        //Animation Timer-------------------------------------
+        private void buttonTimer_Tick(object sender, EventArgs e)
+        {
+            if (chosenDifficulty == "WEAK") 
+            { 
+           easyButton.BackColor = Color.White;
+                Refresh();
+                Thread.Sleep(250);
+                easyButton.BackColor = Color.LightGreen;
+            }
+            if (chosenDifficulty == "COWARDLY")
+            {
+              mediumButton.BackColor = Color.White;
+                Refresh();
+                Thread.Sleep(250);
+                mediumButton.BackColor = Color.LightGoldenrodYellow;
+            }
+            if (chosenDifficulty == "BRAVE") 
+            {
+                hardButton.BackColor = Color.White;
+                Refresh();
+                Thread.Sleep(250);
+                hardButton.BackColor = Color.Red;
+            }
+        }
+
+        //Mouse hovering over buttons--------------------------------
+        private void easyButton_MouseEnter(object sender, EventArgs e)
+        {
+            hardButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Gray;
+            easyButton.BackColor = Color.LightGreen;
+        }
+
+        private void mediumButton_MouseHover(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.LightGoldenrodYellow;
+        }
+
+        private void hardButton_MouseHover(object sender, EventArgs e)
+        {
+            easyButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Red;
+        }
+
+
+        //When buttons are clicked--------------------------------
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            chosenDifficulty = "WEAK";
+            hardButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Gray;
+            loginButton.Visible = true;
+        }
+
+        private void mediumButton_Click(object sender, EventArgs e)
+        {
+            chosenDifficulty = "COWARDLY";
+            easyButton.BackColor = Color.Gray;
+            hardButton.BackColor = Color.Gray;
+            loginButton.Visible = true;
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+            chosenDifficulty = "BRAVE";
+            easyButton.BackColor = Color.Gray;
+            mediumButton.BackColor = Color.Gray;
+            loginButton.Visible = true;
+        }
+
+
+        //Final Login Process!--------------------------------
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            username = usernameInput.Text;
+
+            //player has a path and name
+            if (chosenDifficulty != "Not chosen" && username != "")
+            {
+                outputLabel.Text = $"Welcome {username}.... begin your journey down the path of the {chosenDifficulty}\nG o o d  L u c k";
+                loginButton.Visible = false;
+
+            }
+
+            //player has path
+            else if (username != "") { outputLabel.Text = $"{username}... It seems no journey has chosen you..."; }
+            
+            //player has name
+            else if (chosenDifficulty != "Not chosen") { outputLabel.Text = $"{username}... Are you nameless?"; }
+        }
+    }
+}
